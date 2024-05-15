7360912d9e802595b0e505014c029eccc5cfdc5f
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-05-14 21:02:28 -0300

    initial commit

---
+++ src/api/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
